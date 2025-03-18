32426cd8dccf3bccd213ff492ec18938339fa646
robertd2000 <zdabaghov@mail.ru>
2025-03-18 17:25:27 +0300

    TestNewRepository

--- interval/repository/expense_repository_test.go
+++ interval/repository/expense_repository_test.go
@@ -1,6 +1,7 @@
 package repository
 
 import (
+	"reflect"
 	"testing"
 
 	"github.com/robertd2000/expense-tracker/interval/utils"
@@ -9,8 +10,14 @@ import (
 func TestNewRepository(t *testing.T) {
 	utils.Delete("test.json")
 	
-	repo := NewRepository("test.json")
-	if repo == nil {		
+	got := NewRepository("test.json")
+	if got == nil {		
 		t.Errorf("got nil")
+	}
+
+	want := &repository{sourceFile: "test.json", tasks: nil, lastID: 0}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v, want %v", got, want)
 	}
 }
