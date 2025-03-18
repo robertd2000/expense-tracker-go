beb2de690eefe5cc446383116605c2328f8891f5
robertd2000 <zdabaghov@mail.ru>
2025-03-18 14:10:28 +0300

    want

--- interval/service/expense_test.go
+++ interval/service/expense_test.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"testing"
+	"time"
 
 	"github.com/robertd2000/expense-tracker/interval/models"
 )
@@ -14,7 +15,12 @@ func TestAdd(t *testing.T) {
 	if err != nil {
 		t.Errorf("got nil")
 	}
-	want := models.Expense{}
+	want := models.Expense{
+		ID:      1,
+		Details: "test",
+		Amount:  1.0,
+		Date:    time.Now(),
+	}
 
 	if got != want {
 		t.Errorf("got %v, want %v", got, want)
