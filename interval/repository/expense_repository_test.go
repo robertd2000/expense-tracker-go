0ed5bab57e785585f82a5bcaf3876f20e8ab7d5e
robertd2000 <zdabaghov@mail.ru>
2025-03-18 17:52:27 +0300

    TestAdd

--- interval/repository/expense_repository_test.go
+++ interval/repository/expense_repository_test.go
@@ -3,7 +3,9 @@ package repository
 import (
 	"reflect"
 	"testing"
+	"time"
 
+	"github.com/robertd2000/expense-tracker/interval/models"
 	"github.com/robertd2000/expense-tracker/interval/utils"
 )
 
@@ -20,4 +22,33 @@ func TestNewRepository(t *testing.T) {
 	if !reflect.DeepEqual(got, want) {
 		t.Errorf("got %v, want %v", got, want)
 	}
+}
+
+func TestAdd(t *testing.T) {
+	checkData := func(t testing.TB, got, want interface{}) {
+		t.Helper()
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v, want %v", got, want)
+		}
+	}
+
+	t.Run("add one task", func(t *testing.T) {
+		repo := NewRepository("test.json")
+		repo.Save(*models.NewExpense("test", 1.0))
+		expenses, err := repo.GetAll()
+
+		if err != nil {
+			t.Errorf(err.Error())
+			t.Errorf("got nil")
+		}
+
+		want := &models.Expense{
+			ID:      1,
+			Details: "test",
+			Amount:  1.0,
+			Date:    time.Now(),
+		}
+
+		checkData(t, expenses[0], want)
+	})
 }
