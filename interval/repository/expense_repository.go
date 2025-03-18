80dc12507331e6ed85ac49627739e57f51fd4083
robertd2000 <zdabaghov@mail.ru>
2025-03-18 16:24:45 +0300

    getData

--- interval/repository/expense_repository.go
+++ interval/repository/expense_repository.go
@@ -1,15 +1,49 @@
 package repository
 
-import "github.com/robertd2000/expense-tracker/interval/models"
+import (
+	"github.com/robertd2000/expense-tracker/interval/models"
+	"github.com/robertd2000/expense-tracker/interval/utils"
+)
 
 type Repository interface {
 	Save(expense models.Expense) (models.Expense, error)
 	GetAll() ([]models.Expense, error)
 }
 
-type repository struct {}
+type repository struct {
+	sourceFile string
+}
 
 func NewRepository() Repository {
 	return &repository{}
 }
 
+func (r *repository) Save(expense models.Expense) (models.Expense, error) {
+	return expense, nil
+}
+
+func (r *repository) getData() (*models.ExpenseDB, error) {
+	stream, err := utils.ReadFromJSON(r.sourceFile)
+
+	if err != nil {
+		return nil, err
+	}
+
+	expenseData, err := utils.DeserializeFromJSON[models.ExpenseDB](stream)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &expenseData, nil
+}
+
+func (r *repository) GetAll() ([]models.Expense, error) {
+	expenseData, err := r.getData()
+
+	if err != nil {
+		return nil, err
+	}
+
+	return expenseData.Expenses, nil
+}
