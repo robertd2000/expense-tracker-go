9d594dab3ae7fc27dff2e2aaf10e9407c755a781
robertd2000 <zdabaghov@mail.ru>
2025-03-19 22:04:47 +0300

    NewCommands

--- interval/cli/commands.go
+++ interval/cli/commands.go
@@ -5,3 +5,7 @@ import "github.com/robertd2000/expense-tracker/interval/service"
 type Commands struct {
 	taskService service.ExpenseService
 }
+
+func NewCommands(expenseService service.ExpenseService) *Commands {
+	return &Commands{taskService: expenseService}
+}
