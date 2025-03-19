15c1402fe106e787018fb3cd14f945c38a4f2f5c
robertd2000 <zdabaghov@mail.ru>
2025-03-19 22:07:25 +0300

    commands

--- interval/cli/cli.go
+++ interval/cli/cli.go
@@ -3,9 +3,11 @@ package cli
 import (
 	"fmt"
 	"os"
+
+	"github.com/robertd2000/expense-tracker/interval/service"
 )
 
-func CLI() {
+func CLI(expenseService service.ExpenseService) {
 	args := os.Args[1:]
 
 	if len(args) == 0 {
@@ -13,17 +15,19 @@ func CLI() {
 		return
 	}
 
+	commands := NewCommands(expenseService)
+
 	switch command := args[0]; command {
 	case "add":
 		commands.Add(args)
-	case "update":
-		commands.Update(args)
-	case "delete":
-		commands.Delete(args)
-	case "list":
-		commands.List(args)
-	case "summary":
-		commands.Summary(args)
+	// case "update":
+	// 	commands.Update(args)
+	// case "delete":
+	// 	commands.Delete(args)
+	// case "list":
+	// 	commands.List(args)
+	// case "summary":
+	// 	commands.Summary(args)
 	default:
 		fmt.Println("Invalid command")
 	}
